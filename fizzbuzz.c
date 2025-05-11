4572c0878cb2d53c9cf168467065a9e6dc591128
Kona <konaravindran@gmail.com>
2025-05-11 04:39:45 -0400

    adsasd

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+int main()
+{
+    int max = 100;
+
+    for (int i = 0; i < max, i++)
+    {
+        if (i % 3 == 0):
+        {
+            printf("Fizz");
+        }
+    }
+}
